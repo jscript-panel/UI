230c447b710977a4a0649b267fed7e207a61c7dc
marc2k3 <marc2003@live.co.uk>
2023-12-05 01:56:56 +0000

    Add ColourPickerHook

---
+++ ColourPickerHook.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.hpp"
+#include "ColourPickerHook.hpp"
+
+#include <libPPUI/DarkMode.h>
+
+ColourPickerHook::ColourPickerHook(bool is_dark) : m_is_dark(is_dark) {}
+
+HBRUSH ColourPickerHook::OnCtlColor(CDCHandle dc, CWindow)
+{
+	if (m_is_dark)
+	{
+		static constexpr COLORREF bkColor = 0x202020;
+		static constexpr COLORREF txtColor = 0xC0C0C0;
+
+		dc.SetTextColor(txtColor);
+		dc.SetBkColor(bkColor);
+		dc.SetDCBrushColor(bkColor);
+		return (HBRUSH)GetStockObject(DC_BRUSH);
+	}
+
+	SetMsgHandled(FALSE);
+	return nullptr;
+}
+
+uintptr_t __stdcall ColourPickerHook::HookProc(HWND wnd, uint32_t msg, WPARAM, LPARAM lp)
+{
+	if (msg == WM_INITDIALOG)
+	{
+		CHOOSECOLOR* cc = reinterpret_cast<CHOOSECOLOR*>(lp);
+		reinterpret_cast<modal_dialog_scope*>(cc->lCustData)->initialize(pfc::findOwningPopup(wnd));
+
+		const bool is_dark = ui_config_manager::g_is_dark_mode();
+		PP::subclassThisWindow<ColourPickerHook>(wnd, is_dark);
+		UpdateControls(wnd, is_dark);
+	}
+	return 0;
+}
+
+void ColourPickerHook::UpdateControls(CWindow wnd, bool is_dark)
+{
+	DarkMode::UpdateTitleBar(wnd, is_dark);
+
+	for (CWindow child = wnd.GetWindow(GW_CHILD); child.m_hWnd != nullptr; child = child.GetWindow(GW_HWNDNEXT))
+	{
+		SetWindowTheme(child, is_dark ? L"DarkMode_Explorer" : nullptr, nullptr);
+	}
+}
+
+void ColourPickerHook::ui_colors_changed()
+{
+	m_is_dark = ui_config_manager::g_is_dark_mode();
+
+	Invalidate();
+	UpdateControls(m_hWnd, m_is_dark);
+}
---
+++ ColourPickerHook.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+class ColourPickerHook : public CWindowImpl<ColourPickerHook>, ui_config_callback_impl
+{
+public:
+	ColourPickerHook(bool is_dark);
+
+	BEGIN_MSG_MAP_EX(ColourPickerHook)
+		MSG_WM_CTLCOLORDLG(OnCtlColor)
+		MSG_WM_CTLCOLORSTATIC(OnCtlColor)
+		MSG_WM_CTLCOLOREDIT(OnCtlColor)
+	END_MSG_MAP()
+
+	static uintptr_t __stdcall HookProc(HWND wnd, uint32_t msg, WPARAM, LPARAM lp);
+	static void UpdateControls(CWindow wnd, bool is_dark);
+
+	HBRUSH OnCtlColor(CDCHandle dc, CWindow);
+
+	void ui_colors_changed() final;
+
+private:
+	bool m_is_dark{};
+};
